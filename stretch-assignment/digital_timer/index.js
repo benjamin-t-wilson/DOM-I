61f18b25146faa28cc65007aeab60a9e55025d9b
benjamin-t-wilson <benjiimanwillson@gmail.com>
2019-07-11 17:28:58 -0700

    redid code to be much dryer. Added full functionality and style

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -11,84 +11,55 @@ hunMS.textContent = 0;
 oneS.textContent = 0;
 tenS.textContent = 0;
 
-// let tenMSTimer = window.setInterval(() => {
-//   if (tenMS.textContent < 9) {
-//     tenMS.textContent++;
-//   } else if (tenMS.textContent == 9) {
-//     tenMS.textContent = 0;
-//   }
-// }, 10);
-
-// let hunMSTimer = window.setInterval(() => {
-//   if (hunMS.textContent < 9) {
-//     hunMS.textContent++;
-//   } else if (hunMS.textContent == 9) {
-//     hunMS.textContent = 0;
-//   }
-// }, 100);
-
-// let sTimer = window.setInterval(() => {
-//   if (oneS.textContent < 9) {
-//     oneS.textContent++;
-//   } else if (oneS.textContent == 9) {
-//     oneS.textContent = 0;
-//   }
-// }, 1000);
-
-// window.setTimeout(() => {
-//   clearInterval(tenMSTimer);
-//   clearInterval(hunMSTimer);
-//   clearInterval(sTimer);
-//   tenMS.textContent = 0;
-//   hunMS.textContent = 0;
-//   oneS.textContent = 0;
-//   tenS.textContent = 1;
-//   digits.forEach(cv => {
-//     cv.style.color = "red";
-//   });
-// }, 10000);
-
-startBtn.addEventListener("click", () => {
-  startBtn.style.display = "none";
-  var tens = window.setInterval(() => {
-    if (tenMS.textContent < 9) {
-      tenMS.textContent++;
-    } else if (tenMS.textContent == 9) {
-      tenMS.textContent = 0;
-    }
-  }, 10);
-  var huns = window.setInterval(() => {
-    if (hunMS.textContent < 9) {
-      hunMS.textContent++;
-    } else if (hunMS.textContent == 9) {
-      hunMS.textContent = 0;
-    }
-  }, 100);
-  var ones = window.setInterval(() => {
-    if (oneS.textContent < 9) {
-      oneS.textContent++;
-    } else if (oneS.textContent == 9) {
-      oneS.textContent = 0;
-    }
-  }, 1000);
-  window.setTimeout(() => {
-    clearInterval(tens);
-    clearInterval(huns);
-    clearInterval(ones);
-    tenMS.textContent = 0;
-    hunMS.textContent = 0;
-    oneS.textContent = 0;
-    tenS.textContent = 1;
+var intervalId = null;
+var masterCount = 0;
+function startTime() {
+  digits.forEach(cv => {
+    cv.style.color = "black";
+  });
+  startBtn.style.visibility = "hidden";
+  ++masterCount;
+  var mctens = masterCount % 10;
+  if (masterCount < 9) {
+    tenMS.textContent = masterCount;
+  } else {
+    tenMS.textContent = mctens;
+  }
+  var mchuns = Math.floor(masterCount / 10);
+  if (mchuns > 9) {
+    hunMS.textContent = mchuns % 10;
+  } else {
+    hunMS.textContent = mchuns;
+  }
+  var mcsecs = Math.floor(masterCount / 100);
+  if (mcsecs > 9) {
+    oneS.textContent = mcsecs % 10;
+  } else {
+    oneS.textContent = mcsecs;
+  }
+  var mctensec = Math.floor(masterCount / 1000);
+  tenS.textContent = mctensec;
+  if (mctensec == 1) {
+    clearInterval(intervalId);
     digits.forEach(cv => {
       cv.style.color = "red";
     });
-    startBtn.style.display = "inline-block";
-  }, 10000);
+    startBtn.style.visibility = "visible";
+  }
+}
+startBtn.addEventListener("click", () => {
+  intervalId = window.setInterval(startTime, 10);
 });
 
 resetBtn.addEventListener("click", () => {
-    tenMS.textContent = 0;
-    hunMS.textContent = 0;
-    oneS.textContent = 0;
-    tenS.textContent = 0;
+  clearInterval(intervalId);
+  masterCount = 0;
+  tenMS.textContent = 0;
+  hunMS.textContent = 0;
+  oneS.textContent = 0;
+  tenS.textContent = 0;
+  digits.forEach(cv => {
+    cv.style.color = "black";
+  });
+  startBtn.style.visibility = "visible";
 });
